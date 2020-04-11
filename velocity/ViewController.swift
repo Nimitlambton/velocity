13625a39dcdf601edf3dffda3327c68c1aad9b66
Nimitpamnani <C0764944@mylambton.ca>
2020-04-11 16:44:41 -0400

    the first commit

---
+++ velocity/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  velocity
+//
+//  Created by Nimit on 2020-04-11.
+//  Copyright © 2020 Nimit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .silver    }
+
+
+}
+
