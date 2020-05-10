6f0c154f1ddeaa21ca42dc9c42d5e13f96a2e4f3
Nimitpamnani <C0764944@mylambton.ca>
2020-05-09 22:45:21 -0400

    with 71

---
+++ velocity/controller/SideMenu/MenuController.swift
@@ -0,0 +1,9 @@
+//
+//  menuController.swift
+//  velocity
+//
+//  Created by Nimit on 2020-05-09.
+//  Copyright © 2020 Nimit. All rights reserved.
+//
+
+import Foundation
---
+++ velocity/view/menuHeader.swift
@@ -0,0 +1,9 @@
+//
+//  menuHeader.swift
+//  velocity
+//
+//  Created by Nimit on 2020-05-09.
+//  Copyright © 2020 Nimit. All rights reserved.
+//
+
+import Foundation
